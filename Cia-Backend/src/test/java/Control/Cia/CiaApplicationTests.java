487052fc34298bedab7a3d4eefd4e56a8bc38133
Chaparro10 <19170614@itculiacan.edu.mx>
2023-11-14 14:16:24 -0700

    Se inicializo proyecto en front y backend(se creo la entidad Gasto)

---
+++ Cia-Backend/src/test/java/Control/Cia/CiaApplicationTests.java
@@ -0,0 +1,13 @@
+package Control.Cia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
