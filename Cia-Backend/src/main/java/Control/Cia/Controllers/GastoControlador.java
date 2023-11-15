ac80bf8f6c9dbdb4e4339eedc71cb174eb4d0b1e
Chaparro10 <19170614@itculiacan.edu.mx>
2023-11-15 11:36:45 -0700

    Listado de Gastos desde el controlador REST

--- Cia-Backend/src/main/java/Control/Cia/Controllers/GastoControlador.java
+++ Cia-Backend/src/main/java/Control/Cia/Controllers/GastoControlador.java
@@ -1,14 +1,16 @@
 package Control.Cia.Controllers;
 
-import Control.Cia.service.GastoServicio;
+import Control.Cia.models.Gasto;
 import Control.Cia.service.IGastoServicio;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.logging.Logger;
+import java.util.List;
 
 @RestController
 // La anotación RestController indica que esta clase es un controlador de Spring que maneja solicitudes REST.
@@ -20,13 +22,20 @@ import java.util.logging.Logger;
 // La anotación CrossOrigin se utiliza para permitir solicitudes desde un dominio diferente al dominio del servidor.
 // En este caso, se permite la conexión desde "http://localhost:3000" (puerto típico de desarrollo de aplicaciones front-end).
 public class GastoControlador {
-    // LoggerFactory es una utilidad para obtener instancias de logger.
-    private static final Logger logger = (Logger) LoggerFactory.getLogger(GastoControlador.class);
+   
+    private static final Logger logger = LoggerFactory.getLogger(GastoControlador.class);//Para mandar informacion a consola
 
     @Autowired
     // La anotación Autowired se utiliza para realizar la inyección de dependencias.
     // En este caso, se inyecta una instancia de la interfaz IGastoServicio en esta clase.
     private IGastoServicio gastoServicio;
 
+    //http://localhost:8080/cia-app/Gastos
+    @GetMapping("/Gastos")
+    public List<Gasto> obtenerGastos(){
+        var gastos=gastoServicio.ListarGastos();
+        gastos.forEach(gasto ->logger.info(gasto.toString()));//Para mandar la informacion a la consola
+        return gastos;
+    }
 
 }
